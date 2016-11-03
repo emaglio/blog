8097da5111bf87a46cf56a27d8bc4f783bd44bcd
emaglio <emanuele.magliozzi@gmail.com>
2016-11-03 19:35:27 +1100

    Basic view implemented. List of the ports is shown

--- app/concepts/post/cell/index.rb
+++ app/concepts/post/cell/index.rb
@@ -2,4 +2,15 @@ module Post::Cell
   class Index < Trailblazer::Cell
 
   end
+
+  class Item < Trailblazer::Cell
+    def title
+       model.title
+    end
+
+    def subtitle
+      model.subtitle      
+    end
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def render(cell_constant, operation: @operation, model: @operation.model, options: {})
+    super(html: cell(cell_constant, model,
+      { layout: Blog::Cell::Layout,
+        context: { }
+      }.merge( options)))   
+  end
 end
