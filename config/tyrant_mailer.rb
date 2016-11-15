d6b4f85acffadc07b07c94e664cc1866af95c8af
emaglio <emanuele.magliozzi@gmail.com>
2016-11-15 18:30:41 +1100

    Overide class for reset_password notificatio

---
+++ config/tyrant_mailer.rb
@@ -0,0 +1,16 @@
+Tyrant::Mailer.class_eval do
+  def initialize
+      Pony.options = {
+                      from: "admin@email.com",
+                      via: :smtp, 
+                      via_options: {address: "smtp.gmail.com", 
+                              port: "587",
+                              domain: 'localhost:3000', 
+                              enable_starttls_auto: true, 
+                              # ssl: true, 
+                              user_name: "emanuele.magliozzi@gmail.com", 
+                              password: "lfiwprrihduuivau",
+                              authentication: :plain} 
+                      }
+    end
+end
