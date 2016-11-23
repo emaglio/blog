24414551b48f6fc46d358a5322a2afaf4945a668
emaglio <emanuele.magliozzi@gmail.com>
2016-11-23 12:43:31 +1100

    Edit user without password

---
+++ app/concepts/session/policy.rb
@@ -0,0 +1,17 @@
+class Session::Policy
+  
+  def initialize(user, model)
+    @user = user
+    @model = model
+  end
+
+  def create?
+    true
+  end
+  
+  def current_user?
+    return unless @user
+    @user.email == @model.email
+  end
+
+end
