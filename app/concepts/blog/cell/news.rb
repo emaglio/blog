3345801bf3cd87cb7a1470150344b94d2820ebdd
emasurf85 <emanuele.magliozzi@gmail.com>
2016-12-13 17:09:39 +1100

    Playing with news posts - I don't like it

--- app/concepts/blog/cell/news.rb
+++ app/concepts/blog/cell/news.rb
@@ -1,11 +1,40 @@
 module Blog::Cell
-  class News < Trailblazer::Cell
-    def users
-      return User.last.content["firstname"]
-    end
-    
+
+  module MyPosts
     def posts
-      return Post.last.title
+      post_array = []
+      #get the last 3 posts 
+      # in case there are more than 3
+      if ::Post.all.size < 3
+        post_array << Post.all
+      else
+        i = 0
+        (1..3).each do
+          dim = ::Post.all.size
+          while ::Post.where("id == ?",dim - i).size != 1 or dim-i != 0 do
+            i =+1
+          end
+          post_array << ::Post.find(dim - i)
+          i += 1
+        end
+      end
+      return post_array
+    end
+  end
+
+
+  class News < Trailblazer::Cell
+    include MyPosts
+
+    def show
+      cell(Post, collection: posts)      
+    end
+  
+    class Post < Trailblazer::Cell
+      def link  
+        raise model.inspect
+        link_to model.title, post_path(model)
+      end
     end
   end
 end
--- app/concepts/post/cell/show.rb
+++ app/concepts/post/cell/show.rb
@@ -18,14 +18,18 @@ module Post::Cell
     end
 
     def edit
-      if tyrant.current_user.id.to_s == model.content["user_id"] or tyrant.current_user.email == "admin@email.com" #change in order to have policy
-        link_to "Edit", edit_post_path(model.id)
+      unless tyrant.current_user == nil
+        if tyrant.current_user.id.to_s == model.content["user_id"] or tyrant.current_user.email == "admin@email.com" #change in order to have policy
+          link_to "Edit", edit_post_path(model.id)
+        end
       end
     end
 
     def delete
-      if tyrant.current_user.id.to_s == model.content["user_id"] or tyrant.current_user.email == "admin@email.com" #change in order to have policy
-        link_to "Delete", post_path(model.id), method: :delete, data: {confirm: 'Are you sure?'}
+      unless tyrant.current_user == nil
+        if tyrant.current_user.id.to_s == model.content["user_id"] or tyrant.current_user.email == "admin@email.com" #change in order to have policy
+          link_to "Delete", post_path(model.id), method: :delete, data: {confirm: 'Are you sure?'}
+        end
       end
     end
 
