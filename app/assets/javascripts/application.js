093cfcd13a0407aab94fc5aa1b4f2a38137b27b6
emaglio <emanuele.magliozzi@gmail.com>
2016-12-19 13:47:50 +1100

    Added advanced_search not working for Hash properties #9

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require_tree .
+//= require_tree
--- app/concepts/post/operation/search.rb
+++ app/concepts/post/operation/search.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   class Search < Trailblazer::Operation
+
     def model!(params)
       if params[:advance].include? "true"
         advanced_search(params)
@@ -7,6 +8,7 @@ class Post < ActiveRecord::Base
         search(params)
       end
     end
+
   private
     def search(params)
       if params[:keynote] == nil
@@ -17,55 +19,88 @@ class Post < ActiveRecord::Base
     end
 
     def advanced_search(params)
-      posts_array = Post.all.reverse_order
-      if params[:title] != ""
-        posts_array = search_for_title(posts_array, params[:title].to_s)
+      condition = ""
+      wheres = []
+      multiple_condition = false
+
+      if params["title"] != ""
+        condition = getConditions("title", condition, multiple_condition)
+        wheres << "%" + params["title"].to_s + "%"
+        multiple_condition = true
       end
 
-      if params[:subtitle] != ""
-        posts_array = search_for_subtitle(posts_array, params[:subtitle].to_s)
+      if params["subtitle"] != ""
+        condition = getConditions("subtitle", condition, multiple_condition)
+        wheres << "%" + params["subtitle"].to_s + "%"
+        multiple_condition = true
       end
       
       if params[:body] != ""
-        posts_array = search_for_body(posts_array, params[:body].to_s)
+        condition = getConditions("body", condition, multiple_condition)
+        wheres << "%" + params["body"].to_s + "%"
+        multiple_condition = true
       end
       
       if params[:author] != ""
-        posts_array = search_for_author(posts_array, params[:author].to_s)
+        condition = getConditions("author", condition, multiple_condition)
+        wheres << "%" + params["author"].to_s + "%"
+        multiple_condition = true
       end 
+
+      if condition == ""
+        return ::Post.all.reverse_order
+      else
+        wheres.insert(0, condition)
+        return Post.where(wheres)
+      end
     end
 
-    def search_for_title(model_array, keynote)
-      new_array = []
-      model_array.each do |model|
-        new_array << model if model.title.include? keynote
+    def getConditions(property, condition, multiple_condition)
+      if multiple_condition == true
+        condition = condition + " AND "
       end
-      return new_array
+
+      condition = condition + property.to_s + " like ?"
+      return condition
     end
+
+    def getParams(value, multiple_condition)
+      params = ""
+      params = "%" + value.to_s + "%"
+      return params
+    end
+
+    # def search_for_title(model_array, keynote)
+    #   new_array = []
+    #   model_array.each do |model|
+    #     new_array << model if model.title.include? keynote
+    #   end
+    #   return new_array
+    # end
     
-    def search_for_subtitle(model_array, keynote)
-      new_array = []
-      model_array.each do |model|
-        new_array << model if model.content["subtitle"].include? keynote
-      end
-      return new_array
-    end
+    # def search_for_subtitle(model_array, keynote)
+    #   new_array = []
+    #   model_array.each do |model|
+    #     new_array << model if model.content["subtitle"].include? keynote
+    #   end
+    #   return new_array
+    # end
     
-    def search_for_body(model_array, keynote)
-      new_array = []
-      model_array.each do |model|
-        new_array << model if model.content["body"].include? keynote
-      end
-      return new_array
-    end
+    # def search_for_body(model_array, keynote)
+    #   new_array = []
+    #   model_array.each do |model|
+    #     new_array << model if model.content["body"].include? keynote
+    #   end
+    #   return new_array
+    # end
     
-    def search_for_author(model_array, keynote)
-      new_array = []
-      model_array.each do |model|
-        new_array << model if model.content["author"].include? keynote
-      end
-      return new_array
-    end
+    # def search_for_author(model_array, keynote)
+    #   new_array = []
+    #   model_array.each do |model|
+    #     new_array << model if model.content["author"].include? keynote
+    #   end
+    #   return new_array
+    # end
 
 
   end
