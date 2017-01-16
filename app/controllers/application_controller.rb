3e4cd4b468fd2293755e16138bbdd76d924b43d0
emaglio <emanuele.magliozzi@gmail.com>
2017-01-17 09:23:29 +1100

    Need to use the new render format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def render(cell_constant, operation: @result, model: @result["model"], options: {})
-    super(html: cell(cell_constant, model,
-      { layout: Blog::Cell::Layout,
-        context: {tyrant: tyrant, policy: operation.policy, flash: flash}
-      }.merge( options)))   
-  end
+  # def render(cell_constant, operation: @result, model: @result["model"], options: {})
+  #   super(html: cell(cell_constant, model,
+  #     { layout: Blog::Cell::Layout,
+  #       context: {tyrant: tyrant, policy: operation.policy, flash: flash}
+  #     }.merge( options)))   
+  # end
 
   def tyrant
     Tyrant::Session.new(request.env['warden'])
