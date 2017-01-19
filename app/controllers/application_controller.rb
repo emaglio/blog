dd37038fb932656bc44d207590c6ad03104a08ca
emasurf85 <emanuele.magliozzi@gmail.com>
2017-01-19 14:10:20 +1100

    Find_by works - must be tested #18

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # def render(cell_constant, operation: @result, model: @result["model"], options: {})
-  #   super(html: cell(cell_constant, model,
-  #     { layout: Blog::Cell::Layout,
-  #       context: {tyrant: tyrant, policy: operation.policy, flash: flash}
-  #     }.merge( options)))   
-  # end
-
   def tyrant
     Tyrant::Session.new(request.env['warden'])
   end
-
-  # def process_params!(params)
-  #   params.merge!(current_user: tyrant.current_user)
-  # end
 
   class NotAuthorizedError < RuntimeError
   end
@@ -24,5 +13,10 @@ class ApplicationController < ActionController::Base
   rescue_from ApplicationController::NotAuthorizedError do |exception|
     flash[:alert] = "You are not authorized mate!"
     redirect_to posts_path
+  end
+
+private
+  def _run_options(options)
+    options.merge("current_user" => tyrant.current_user )
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,17 @@
 class PostsController < ApplicationController
 
   def show
-    run Post::Show, current_user: tyrant.current_user, params: params
+    run Post::Show
     render cell(Post::Cell::Show, result["model"], context: { current_user: tyrant.current_user, flash: flash }, layout: Blog::Cell::Layout)
   end
   
   def index
-    run Post::Index, current_user: tyrant.current_user
+    run Post::Index
     render cell(Post::Cell::Index, result["model"], context: { current_user: tyrant.current_user, flash: flash }, layout: Blog::Cell::Layout)
   end
 
   def create
-    run Post::Create, current_user: tyrant.current_user do |result|
+    run Post::Create do |result|
       flash[:notice] = "#{result["model"].title} has been created"
       return redirect_to "/posts"
     end
@@ -19,17 +19,17 @@ class PostsController < ApplicationController
   end
 
   def new
-    run Post::New, current_user: tyrant.current_user
+    run Post::New
     render cell(Post::Cell::New, result["model"], context: { current_user: tyrant.current_user, flash: flash }, layout: Blog::Cell::Layout)
   end
 
   def edit
-    run Post::Edit, current_user: tyrant.current_user
+    run Post::Edit
     render cell(Post::Cell::Edit, result["model"], context: { current_user: tyrant.current_user, flash: flash }, layout: Blog::Cell::Layout)
   end
 
   def update
-    run Post::Update, current_user: tyrant.current_user do |result|
+    run Post::Update do |result|
       flash[:notice] = "#{result["model"].title} has been saved"
       return redirect_to "/posts/#{result["model"].id}"
     end
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    run Post::Delete, current_user: tyrant.current_user do
+    run Post::Delete do
       flash[:alert] = "Post deleted"
       return redirect_to "/posts"
     end
